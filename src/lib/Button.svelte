b26a81934a1196f6c0725de7a20127ff762d7ba2
zztkm <zztkm@tsurutatakumi.info>
2023-06-09 01:27:56 +0900

    とりあえず svelte kit

---
+++ src/lib/Button.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	function toggle() {
+		window.document.body.classList.toggle('dark-mode');
+	}
+</script>
+
+<button on:click={toggle}>
+	<slot />
+</button>
+
+<style>
+	button {
+		background-color: #f5beaa;
+		color: white;
+		border: none;
+		border-radius: 4px;
+		padding: 0.5rem;
+	}
+	:global(body.dark-mode) button {
+		background-color: #0084f6;
+		color: white;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Button from '../lib/Button.svelte';
+</script>
+
+<Button>🤔</Button>
+
+<slot />
+
+<style>
+	:global(body) {
+		background-color: #f2eee2;
+		color: #0084f6;
+		transition: background-color 0.3s;
+	}
+	:global(body.dark-mode) {
+		background-color: #1d3040;
+		color: #bfc2c7;
+	}
+</style>
