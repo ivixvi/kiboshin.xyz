e473f252765173b9d1e44ebf153769161da696df
zztkm <zztkm@tsurutatakumi.info>
2023-06-09 13:00:00 +0900

    rotate kobochan

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,18 @@
 <h1>Welcome to kiboshin kingdom!</h1>
-<p>
-	<img src="kobochan.webp" width="300" height="300" alt="kobochan" loading="lazy" />
-</p>
+
+	<div class="box1">
+		<img src="kobochan.webp" width="300" height="300" alt="kobochan" loading="lazy" />
+	</div>
 
 <nobr><table border="0" cellspacing="0" cellpadding="0"><tbody><tr><td><a href="https://www.free-counter.jp/"><img src="https://www.f-counter.net/ani1/62/1686281428/" alt="アクセスカウンター" border="0" style="margin:0px; padding:0px; border:0px; vertical-align:bottom"></a></td>
 <td><a href="https://www.free-counter.jp/"><img src="https://www.f-counter.net/ani2/62/1686281428/" alt="アクセスカウンター" border="0" style="margin:0px; padding:0px; border:0px; vertical-align:bottom"></a></td></tr></tbody></table></nobr>
 
+<style>
+.box1{
+  animation:3s linear infinite rotation;
+}
+@keyframes rotation{
+  0%{ transform:rotate(0);}
+  100%{ transform:rotate(360deg); }
+}
+</style>
